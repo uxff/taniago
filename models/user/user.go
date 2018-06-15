6f3ab4506227838350f68884ccfea6248f899ac5
uxff <uxff@qq.com>
2018-06-15 08:38:50 +0800

    add user controller

---
+++ models/user/user.go
@@ -0,0 +1,45 @@
+package user
+
+import "time"
+
+// email unique
+
+type UserEntity struct {
+	Email string	// email unique
+	Username string	// user name not unique
+	Password string
+	Phone string
+
+	RegisterTime time.Time
+	RegisterIp string
+
+	LastLoginTime time.Time
+	LastLoginIp string
+}
+
+
+// interface for controller or biz
+// for register
+func Create(e *UserEntity) error {
+	return nil
+}
+
+// for login
+func CheckPwd(email, pwd string) error {
+	return nil
+}
+
+
+func GetByEmail(email string) (*UserEntity, error) {
+	return nil, nil
+}
+
+
+func ValidateEmail(verifyCode string) error {
+	return nil
+}
+
+func SendValidateEmailCode(email string) error {
+	return nil
+}
+
