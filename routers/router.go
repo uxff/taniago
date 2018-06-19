a74b527def8b6ca9477a4722e43b2e7132f059d7
uxff <uxff@qq.com>
2018-06-19 23:27:23 +0800

    update router

--- routers/router.go
+++ routers/router.go
@@ -10,10 +10,10 @@ func init() {
 	beego.Router("/", &controllers.IndexController{}, "*:Index")
 	beego.Router("/index", &controllers.IndexController{}, "*:Index")
 	beego.Router("/picset/*", &controllers.IndexController{}, "*:Picset")
-	beego.Router("/main", &controllers.BaseController{}, "*:Index")
-	beego.Router("/login", &controllers.BaseController{}, "*:Login")
-	beego.Router("/logout", &controllers.BaseController{}, "*:Logout")
 	beego.Router("/payment", &controllers.PaysapiController{}, "*:Payment")
 	beego.Router("/notify", &controllers.PaysapiController{}, "*:Notify")
 	beego.Router("/paymentstatus", &controllers.PaysapiController{}, "*:PaymentStatus")
+	beego.Router("/login", &controllers.LoginController{}, "get,post:Login")
+	beego.Router("/logout", &controllers.LoginController{}, "get:Logout")
+	beego.Router("/signup", &controllers.LoginController{}, "get,post:Signup")
 }
